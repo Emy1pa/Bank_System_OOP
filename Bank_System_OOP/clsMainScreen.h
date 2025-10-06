ac7771f627e00505d2347aa63bbebd707fdf9c2c
Emy1pa <souaouti.iman@gmail.com>
2025-10-06 15:39:15 +0100

    feat/update: Add Delete Client Screen and integrate with Main Menu
    - Implemented clsDeleteClientScreen to handle client deletion with confirmation.
    - Added clsDeleteClientScreen::ShowDeleteClientScreen() call inside existing _ShowDeleteClientScreen() function.
    - Displays client details before deletion for user confirmation.
    - Validates account number existence before attempting deletion.
    - Shows success or error message based on the deletion result.

--- Bank_System_OOP/clsMainScreen.h
+++ Bank_System_OOP/clsMainScreen.h
@@ -5,6 +5,7 @@
 #include "clsInputValidate.h"
 #include "clsClientListScreen.h"
 #include "clsAddNewClientScreen.h"
+#include "clsDeleteClientScreen.h"
 
 using namespace std;
 
@@ -42,7 +43,8 @@ private:
 
     static void _ShowDeleteClientScreen()
     {
-        cout << "\nDelete Client Screen Will be here...\n";
+        //cout << "\nDelete Client Screen Will be here...\n";
+        clsDeleteClientScreen::ShowDeleteClientScreen();
     }
 
     static void _ShowUpdateClientScreen()
